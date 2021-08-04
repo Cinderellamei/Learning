c91e9c9ea792ade68ad00b530f012c4bb2ee65f6
Ao_xue_mei@163.com <gonglei.leo@bytedance.com>
2021-08-04 22:18:54 +0800

    update

--- src/main/java/Algorithm/leecode/bytedance/CoderPractice.java
+++ src/main/java/Algorithm/leecode/bytedance/CoderPractice.java
@@ -265,6 +265,40 @@ public class CoderPractice {
         return result;
     }
 
+    /**
+    * 相交链表
+    * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 
+    **/
+    public ListNode getIntersectionNode(ListNode headA,ListNode headB) {
+        if(headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while(pA != pB) {
+            pA = pA == null?headB:pA.next;
+            pB = pB ==null?headA:pB.next;
+        }
+        return pA;
+    }
 
-
+    /**
+    * 买卖股票的最佳时机
+    * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+    * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+    * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+    **/
+    public int maxProfit(int [] prices) {
+        int maxProfit = 0;
+        int minPricecs = prices[0];
+        for(int i = 1;i<prices.length;i++) {
+            if(prices[i] < minProfit) {
+                maxProfit = prices[i];
+            }
+            if(prices[i]-minPrices > maxProfit) {
+                maxProfit = prices[i]-minPrices;
+            }
+        }
+        return maxProfit;
+    } 
 }
--- src/main/java/Algorithm/leecode/bytedance/LRUCache.java
+++ src/main/java/Algorithm/leecode/bytedance/LRUCache.java
@@ -4,15 +4,77 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LRUCache {
+    
+    public class DLinkedNode{
+    	int key;
+    	int value;
+    	DLinkedNode pre;
+    	DLinkedNOde next;
+    	public DLinkedNode(){}
+    	public DLinkedNode(int key,int value) {
+    		this.key = key;
+    		this.value = value;
+    	}
+    }
+
+    private Map<Integer,DLinkedNode> cache = new HashMap<>();
 
     public LRUCache(int capacity) {
+    	this.capacity = capacity;
+    	head = new DLinkedNode();
+    	tail = new DLinkedNode(); 
+    	head.next = tail;
+    	tail.pre = head;
     }
 
     public int get(int key) {
-        return -1;
+    	DLinkedNode node = cache.get(key);
+    	if(node == null) {
+    		return -1;
+    	} else {
+    		return node.val;
+    	}
     }
 
     public void put(int key,int value) {
+    	DLinkedNode node = cache.get(key) {
+    		if(node == null) {
+    			DLinkedNode tempNode = new DLinkedNode(value);
+    			cache.put(key,tempNode);
+    			addToHead(tempNode);
+    			size++;
+    			if(size > capacity) {
+    				DLinkedNode temp = removeTail();
+    				cache.remove(temp.key);
+    				size--;
+    			}
+    		} else {
+    			node.value = value;
+    			moveToHead(node);
+    		}
+    	}
     }
 
+    private void addToHead(DLinkedNode node) {
+    	node.next = head.next;
+    	node.pre = head;
+    	head.next.pre = node;
+    	head.next = node;
+    }
+
+    private void removeNode(DLinkedNode node) {
+    	node.pre.next = node.next;
+    	node.next.pre = node.pre;    	
+    }
+
+    private void moveToHead(DLinkedNode node) {
+    	removeNode(node);
+    	addToHead(node);
+    }
+
+    private DLinkedNode removeTail() {
+    	DLinkedNode cur = tail.pre;
+    	removeNode(cur);
+    	return cur;
+    }
 }
